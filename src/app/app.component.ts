33200f07783c0a3bf476bbf4d321320dc2c6ec03
Vladislav <amen192315@gmail.com>
2025-04-25 18:23:57 +0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,62 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, OnInit } from "@angular/core";
+import { FormsModule } from "@angular/forms";
+import { CommonModule, NgClass, NgFor } from '@angular/common';
 
-@Component({
-  selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
-})
-export class AppComponent {
-  title = 'teach-Angular';
+export interface ITodoItem{
+    id: number,
+    task: string,
+    completed: boolean
 }
+
+  
+@Component({
+    selector: "app-root",
+    standalone: true,
+    imports: [FormsModule, CommonModule, NgFor, NgClass], // Добавили CommonModule
+    templateUrl: './app.component.html', // Указали правильный путь к шаблону
+    styleUrls: ['./app.component.scss'] // Опционально, если есть стили
+})
+export class AppComponent implements OnInit {
+    
+    todoList: ITodoItem[] = []
+    newTask: string = ''
+    
+    private readonly STORAGE_KEY = 'angular_todo_list'
+
+    ngOnInit(): void {
+        this.loadTasks()
+    }
+
+    private loadTasks(): void {
+        const savedTasks = localStorage.getItem(this.STORAGE_KEY)
+        if(savedTasks) {
+            this.todoList = JSON.parse(savedTasks)
+        }
+    }
+
+    private saveTasks(): void {
+        localStorage.setItem(this.STORAGE_KEY, JSON.stringify(this.todoList))
+    }
+
+    addTask():void{
+        if(this.newTask.trim() !== ''){
+            const newTodoItem : ITodoItem = {
+                id: Date.now(),
+                task: this.newTask,
+                completed: false
+            }
+            this.todoList.push(newTodoItem)
+            this.newTask = ''
+            this.saveTasks()
+        }
+    }
+
+    toggleCompleted(index: number): void {
+        this.todoList[index].completed = !this.todoList[index].completed
+        this.saveTasks()
+    }
+    deleteTask(id: number) {
+        this.todoList = this.todoList.filter(item => item.id !== id)
+        this.saveTasks()
+    }
+ }
